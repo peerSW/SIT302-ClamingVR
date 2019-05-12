16fba5ffc679bcc970d0c25ea1a1f509432f708b
EricHo1997 <barathrum204@gmail.com>
2019-05-12 21:06:15 +1000

    Main Menu

--- 374--beach-master/Assets/Scripts/GameController.cs
+++ 374--beach-master/Assets/Scripts/GameController.cs
@@ -5,17 +5,28 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
     public void NextScene()
     {
         SceneManager.LoadScene(1);
     }
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void NextScene2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
